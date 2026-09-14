bc6ed6b7a7011004b790e58ec1e3f6c96ffc96c2
Callista Christer <362686198@GoTVDSB.ca>
2026-09-14 12:42:38 -0400

    Add project files.

---
+++ Topic 3 - Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_3___Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
