911cb2399964ac56b23bef7e75e4873e9d50591b
Callista Christer <362686198@GoTVDSB.ca>
2026-09-18 12:48:03 -0400

    h

--- Topic 3 - Variables/Program.cs
+++ Topic 3 - Variables/Program.cs
@@ -56,6 +56,7 @@ namespace Topic_3___Variables
             Console.WriteLine();
             Console.WriteLine($"My name is {myName} and I'll graduate in {gradYear}");
             Console.WriteLine();
+            Console.WriteLine($"The area of a circle with the radius of {radius} is" + (pi * (radius * radius)));
         }
     }
 }
